1967a05fb6ff3fe9fa558e10c25a6c5e85dd162a
Francesca Giudici <francesca.giudici@mondora.com>
2017-10-27 16:51:36 +0200

    [chore]: add css to facts section

--- src/components/FactCard/index.js
+++ src/components/FactCard/index.js
@@ -1,6 +1,6 @@
 import {Col} from 'antd';
 import React, {Component} from 'react';
-import {white} from '../../commons/colors';
+import * as colors from '../../commons/colors';
 
 import PropTypes from 'prop-types';
 
@@ -8,6 +8,7 @@ export default class Card extends Component {
 
     static propTypes = {
         icon: PropTypes.object,
+        lg: PropTypes.number,
         md: PropTypes.number,
         text: PropTypes.string,
         title: PropTypes.string,
@@ -15,25 +16,44 @@ export default class Card extends Component {
     }
 
     render () {
-        const {icon, md, text, title, xs} = this.props;
+        const {icon, lg, md, text, title, xs} = this.props;
         return (
             <Col
+                lg={lg}    
                 xs={xs}
                 md={md}
-                style={{
-                    backgroundColor: white,
-                    borderRadius: 10,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    height: 200,
-                    width: 200,
-                    margin: 5
-                }}
             >
-                <img alt={'fact'} style={{height: 80}} src={icon} />
-                <div style={{fontSize: 30}}>{title}</div>
-                <div style={{fontSize: 14, textAlign: 'center'}}>{text}</div>
+                <div
+                    style={{
+                        backgroundColor: colors.white,
+                        borderRadius: 10,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        minHeight: 250,
+                        margin: 20,
+                        padding: 20,
+                        WebkitBoxShadow: '0px 8px 60px -9px rgba(28, 27, 41, 0.3)',
+                        MozBoxShadow: '0px 8px 60px -9px rgba(28, 27, 41, 0.3)',
+                        boxShadow: '0px 8px 60px -9px rgba(28, 27, 41, 0.3)'
+                    }}
+                > 
+                    <img alt={'fact'} style={{height: 'calc(50px + 2vw)'}} src={icon} />
+                    <div
+                        style={{
+                            fontSize: 'calc(22px + 0.3vw)', fontWeight: 500, color: colors.black
+                        }}
+                    >
+                        {title}
+                    </div>
+                    <div
+                        style={{
+                            fontSize: 'calc(12px + 0.3vw)', textAlign: 'center', color: colors.grey
+                        }}
+                    >
+                        {text}
+                    </div>
+                </div>
             </Col>
         );
     }
--- src/components/sections/factsAndFigures/index.js
+++ src/components/sections/factsAndFigures/index.js
@@ -3,70 +3,58 @@ import React, {Component} from 'react';
 import Card from '../../FactCard/index';
 import {Row} from 'antd';
 
-import gradient from '../../../assets/images/gradient_facts.png';
 import bike from '../../../assets/images/icons/icon_bike.svg';
 import calendar from '../../../assets/images/icons/icon_calendar.svg';
 import heart from '../../../assets/images/icons/icon_heart.svg';
 import world from '../../../assets/images/icons/icon_world.svg';
 
+import {style} from './style';
+
 export default class FactsAndFigures extends Component {
 
     render () {
         return (
-            <Row
-                style={{
-                    backgroundImage: `url(${gradient})`,
-                    backgroundSize: 'contain',
-                    backgroundRepeat: 'repeat-y',
-                    paddingBottom: 50,
-                    paddingTop: 50
-                }}
-            >
-                <div style={{margin: 'auto', maxWidth: 1200}}>
-                    <h2
-                        style={{
-                            marginBottom: 20,
-                            fontSize: 30,
-                            textAlign: 'center'
-                        }}
-                    >
+            <div style={style.container} >
+                <Row style={style.customRow}>
+                    <h2 style={style.sectionTitle}>
                         {'Facts and Figures'}
                     </h2>
-                    <Row
-                        type={'flex'}
-                        justify={'space-around'}
-                    >
+                    <Row gutter={40}>
                         <Card
                             icon={bike}
-                            md={6}
+                            lg={6}
+                            md={12}
                             text={'average length of employee commute per day'}
                             title={'35 km'}
                             xs={24}
                         />
                         <Card
                             icon={calendar}
-                            md={6}
+                            lg={6}
+                            md={12}
                             text={'CO2 per person per day generated through commuting'}
                             title={'4.32 kg'}
                             xs={24}
                         />
                         <Card
                             icon={world}
-                            md={6}
+                            lg={6}
+                            md={12}
                             text={'CO2 per person per year offset by cycling'}
                             title={'1 tonne'}
                             xs={24}
                         />
                         <Card
                             icon={heart}
-                            md={6}
+                            lg={6}
+                            md={12}
                             text={'average health and wellbeing improvement from cycling'}
                             title={'40%'}
                             xs={24}
                         />
                     </Row>
-                </div>
-            </Row>
+                </Row>
+            </div>
         );
     }
 
