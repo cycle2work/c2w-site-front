07db3d0fef674efaca1fab13eefae21590fdad72
Francesca Giudici <francesca.giudici@mondora.com>
2017-10-27 14:37:33 +0200

    [chore]: add principal colors

--- src/commons/colors.js
+++ src/commons/colors.js
@@ -1,5 +1,28 @@
-export const blue = '#2ac1cd';
+// Common styles
+export const background = '#ffffff';
+export const backgroundError = '#ebccd1';
+export const errorText = '#a94442';
 export const white = '#ffffff';
-export const yellow = 'yellow';
-export const black = '#000000';
+export const transparent = 'transparent';
+export const lighterGrey = '#E9EDF3';
+export const lightGrey = '#C3CEDB';
 export const grey = '#95a1b5';
+export const backToTop = '#7E8389';
+export const scrollToBottom = '#ffffff';
+export const silver = '#2E343D';
+export const darkGrey = '#171B23';
+export const black = '#000000';
+export const blackOpacity = 'rgba(0, 0, 0, 0.9)';
+export const bgModalOpacity = 'rgba(0, 0, 0, 0.7)';
+
+// Site Style
+export const primaryColorLighter = '#D4EDFF';
+export const primaryColorLight = '#2ac1cd';
+export const primaryColor = '#2ac1cd';
+export const primaryColorDarker = '#19508C';
+export const secondaryColor = '#FFEA00';
+export const tertiaryColor = '#E62F72';
+export const green = '#06bc53';
+export const red = '#cc0000';
+export const backgroundFooter = '#232934';
+export const backgroundBottomFooter = '#0c0e11';
