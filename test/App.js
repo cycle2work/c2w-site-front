2026ce3d297f33ae9a98635536174d0d6017b4a9
trincadev <alessandro.trinca@mondora.com>
2017-11-02 19:57:12 +0100

    fix broken test

--- test/App.js
+++ test/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
+import {Provider} from 'react-redux';
 
 import App from '../src/App';
+import store from '../src/store';
+
 
 describe('App', () => {
 
     it('renders', () => {
-        const element = shallow(<App />);
+        const element = shallow(
+            <Provider store={store}>
+                <App />
+            </Provider>
+        );
         expect(element).to.have.length(1);
     });
 
