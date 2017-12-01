65fc1073813c93554a26e87f1c803271b5a27a6b
Manuel Serra <nitroin@hotmail.com>
2017-12-01 21:54:01 +0100

    [chore]: fetch previous month

--- src/actions/reports.js
+++ src/actions/reports.js
@@ -7,7 +7,7 @@ export const FETCH_REPORTS_START = "FETCH_REPORTS_START";
 export const FETCH_REPORTS_SUCCESS = "FETCH_REPORTS_SUCCESS";
 export const FETCH_REPORTS_ERROR = "FETCH_REPORTS_ERROR";
 
-export function fetchReports(month = moment.utc().format("MM")) {
+export function fetchReports(month = moment.utc().subtract({ month: 1 }).format("MM")) {
     return dispatch => {
         dispatch({
             type: FETCH_REPORTS_START
