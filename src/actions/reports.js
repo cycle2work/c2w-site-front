9074ab12e4db9c24506cc50e00cf3b51d8c5c93a
Manuel Serra <nitroin@hotmail.com>
2018-05-12 10:16:33 +0200

    [chore]: remove DownloadLink

--- src/actions/reports.js
+++ src/actions/reports.js
@@ -7,22 +7,25 @@ export const FETCH_REPORTS_START = "FETCH_REPORTS_START";
 export const FETCH_REPORTS_SUCCESS = "FETCH_REPORTS_SUCCESS";
 export const FETCH_REPORTS_ERROR = "FETCH_REPORTS_ERROR";
 
-export function fetchReports(month = moment.utc().subtract({ month: 1 }).format("MM")) {
+export function fetchReports(month = moment.utc().format("MM")) {
     return dispatch => {
         dispatch({
             type: FETCH_REPORTS_START
         });
 
-        axios.get(`${AWS_REPORTS_API}?month=${month}`).then(result => {
-            dispatch({
-                type: FETCH_REPORTS_SUCCESS,
-                payload: result.data
+        axios
+            .get(`${AWS_REPORTS_API}?month=${month}`)
+            .then(result => {
+                dispatch({
+                    type: FETCH_REPORTS_SUCCESS,
+                    payload: result.data
+                });
+            })
+            .catch(error => {
+                dispatch({
+                    type: FETCH_REPORTS_ERROR,
+                    payload: error
+                });
             });
-        }).catch((error) => {
-            dispatch({
-                type: FETCH_REPORTS_ERROR,
-                payload: error
-            });
-        });
     };
 }
