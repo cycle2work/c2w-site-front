ae907b5add527230b852d860eed1241d321a4fee
Manuel Serra <nitroin@hotmail.com>
2019-07-17 09:54:39 +0200

    [chore]: update Kg CO2 formulas

--- src/libs/stats.js
+++ src/libs/stats.js
@@ -22,11 +22,12 @@ export const getStats = activities => {
 
     const delta = total - totalPrevious;
 
+    // https://ecf.com/files/wp-content/uploads/ECF_BROCHURE_EN_planche.pdf?origin_team=T0280AVS0
     const stats = {
         km: total / 1000,
         deltaKm: delta / 1000,
-        co2: total / 600000,
-        deltaCo2: delta / 600000,
+        co2: total / 1000 * 0.229,
+        deltaCo2: delta / 1000 * 0.229,
         euro: total * 0.2 / 1000,
         deltaEuro: delta * 0.2 / 1000,
         more
