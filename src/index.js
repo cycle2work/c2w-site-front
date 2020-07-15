10ca19eb2f0750f3fb45b1546647bee19c9e9cae
luzzif <fedeluzzi00@gmail.com>
2020-07-15 16:45:17 +0200

    [feat]: setup i18n and translate header menu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
+import { IntlProvider } from "react-intl";
 
 import Views from "./views";
-
+import en from "./localization/messages/en.json";
+import it from "./localization/messages/it.json";
+import { getLanguage } from "./localization";
 import "./index.css";
 
-render(<Views />, document.getElementById("root"));
+const language = getLanguage();
+const messages = {
+    en,
+    it
+};
+
+render(
+    <IntlProvider locale={language} messages={messages[language]}>
+        <Views />
+    </IntlProvider>,
+    document.getElementById("root")
+);
