c500c2a4951e48462f4911488c7652fd01c5194b
luzzif <fedeluzzi00@gmail.com>
2020-07-15 17:19:24 +0200

    [fix]: load moment locales

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import { render } from "react-dom";
 import { IntlProvider } from "react-intl";
 
 import Views from "./views";
+import momentIt from "moment/locale/it";
 import en from "./localization/messages/en.json";
 import it from "./localization/messages/it.json";
 import { getLanguage } from "./localization";
 import "./index.css";
+import moment from "moment";
+
+// setting up moment locales
+moment.locale("it", momentIt);
 
 const language = getLanguage("en");
 const messages = {
