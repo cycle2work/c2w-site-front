e5ec0fc361b39ac7ea2e17cbf4fe6490c807225d
nitroin <nitroin@hotmail.com>
2021-05-26 08:18:54 +0200

    chore: add antd locale

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
 import { IntlProvider } from "react-intl";
+import moment from "moment";
 
-import Views from "./views";
 import momentIt from "moment/locale/it";
 import en from "./localization/messages/en.json";
 import it from "./localization/messages/it.json";
 import { getLanguage } from "./localization";
+
+import { LocaleProvider } from "antd";
+import antdEn from "antd/lib/locale-provider/en_US";
+import antdIt from "antd/lib/locale-provider/it_IT";
+
+import Views from "./views";
+
 import "./index.css";
-import moment from "moment";
 
 // setting up moment locales
 moment.locale("it", momentIt);
@@ -16,12 +22,19 @@ moment.locale("it", momentIt);
 const language = getLanguage("en");
 const messages = {
     en,
-    it
+    it,
+};
+
+const antdLocale = {
+    en: antdEn,
+    it: antdIt,
 };
 
 render(
     <IntlProvider locale={language} messages={messages[language]}>
-        <Views />
+        <LocaleProvider locale={antdLocale[language]}>
+            <Views />
+        </LocaleProvider>
     </IntlProvider>,
     document.getElementById("root")
 );
